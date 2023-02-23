427f13d3e735440d3b70a43f64ccc2041234b8e6
Philipp Samfass <philipp.samfass@amd.com>
2023-02-23 14:53:11 +0100

    apply workaround for HIPCC issue, so that all "fake main" functions are compiled for both host and device

--- .upstream-tests/test/force_include_hip.h
+++ .upstream-tests/test/force_include_hip.h
@@ -8,6 +8,23 @@
 //
 //===----------------------------------------------------------------------===//
 
+// Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 // We use <stdio.h> instead of <iostream> to avoid relying on the host system's
 // C++ standard library.
 #include <stdio.h>
@@ -91,5 +108,9 @@ int main(int argc, char** argv)
     return ret;
 }
 
+#if defined(__HIP_PLATFORM_AMD__)
+#define main __device__ __host__ fake_main
+#else
 #define main fake_main
+#endif
 
