f8f016f2aac6180e5c60a27b43794c58f298b0a1
Philipp Samfass <philipp.samfass@amd.com>
2023-03-13 17:10:20 +0100

    remove nvcc-lit-note and fixed 5 more tests (low-hanging fruits), made two tests that require more investigation unsupported for now

--- .upstream-tests/test/std/utilities/meta/meta.trans/meta.trans.other/underlying_type.fail.cpp
+++ .upstream-tests/test/std/utilities/meta/meta.trans/meta.trans.other/underlying_type.fail.cpp
@@ -5,6 +5,24 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+
+// Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 // UNSUPPORTED: c++98, c++03, c++11, C++14, c++17
 // type_traits
 
@@ -21,7 +39,7 @@ enum E1
   E1Zero,
   E1One,
   E1Two = sizeof(cuda::std::underlying_type<E1>::type)
-}; // expected-error@type_traits:* {{cannot determine underlying type of incomplete enumeration type 'E1'}}
+}; // expected-error@*:* {{cannot determine underlying type of incomplete enumeration type 'E1'}}
 
 //  None of these are incomplete.
 //  Scoped enums have an underlying type of 'int' unless otherwise specified
--- .upstream-tests/test/std/utilities/tuple/tuple.tuple/tuple.cnstr/alloc.fail.cpp
+++ .upstream-tests/test/std/utilities/tuple/tuple.tuple/tuple.cnstr/alloc.fail.cpp
@@ -6,7 +6,25 @@
 //
 //===----------------------------------------------------------------------===//
 
+// Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 // UNSUPPORTED: c++98, c++03
+// TODO (hipcc): fix following compiler error: /usr/lib/gcc/x86_64-linux-gnu/9/../../../../include/c++/9/tuple:699:2: note: candidate constructor template not viable: no known conversion from 'const cuda::std::allocator_arg_t' to 'std::allocator_arg_t' for 1st argument tuple(allocator_arg_t __tag, const _Alloc& __a)
 
 // <cuda/std/tuple>
 
