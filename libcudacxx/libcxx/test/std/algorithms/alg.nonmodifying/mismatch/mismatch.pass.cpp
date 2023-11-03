frankdmacias/cccl

Move `std::mismatch` into its own file

libcudacxx/libcxx/test/std/algorithms/alg.nonmodifying/mismatch/mismatch.pass.cpp
@@ -23,6 +23,10 @@
 #include "test_macros.h"
 #include "test_iterators.h"
 
+#ifdef TEST_COMPILER_MSVC_2017
+#pragma warning(disable: 4018) // signed/unsigned mismatch
+#endif // TEST_COMPILER_MSVC_2017
+
 #if TEST_STD_VER > 17
 TEST_CONSTEXPR bool test_constexpr() {
     int ia[] = {1, 3, 6, 7};

libcudacxx/libcxx/test/std/algorithms/alg.nonmodifying/mismatch/mismatch_pred.pass.cpp
@@ -26,6 +26,10 @@
 #include "test_iterators.h"
 #include "counting_predicates.h"
 
+#ifdef TEST_COMPILER_MSVC_2017
+#pragma warning(disable: 4018) // signed/unsigned mismatch
+#endif // TEST_COMPILER_MSVC_2017
+
 #if TEST_STD_VER > 17
 TEST_CONSTEXPR bool eq(int a, int b) { return a == b; }
 

ROCm/libhipcxx

libcudacxx/libcxx/test/std/algorithms/alg.nonmodifying/mismatch/mismatch.pass.cpp
@@ -23,6 +23,10 @@
 #include "test_macros.h"
 #include "test_iterators.h"
 
+#ifdef TEST_COMPILER_MSVC
+#pragma warning(disable: 4018) // signed/unsigned mismatch
+#endif // TEST_COMPILER_MSVC
+
 #if TEST_STD_VER > 17
 TEST_CONSTEXPR bool test_constexpr() {
     int ia[] = {1, 3, 6, 7};

libcudacxx/libcxx/test/std/algorithms/alg.nonmodifying/mismatch/mismatch_pred.pass.cpp
@@ -26,6 +26,10 @@
 #include "test_iterators.h"
 #include "counting_predicates.h"
 
+#ifdef TEST_COMPILER_MSVC
+#pragma warning(disable: 4018) // signed/unsigned mismatch
+#endif // TEST_COMPILER_MSVC
+
 #if TEST_STD_VER > 17
 TEST_CONSTEXPR bool eq(int a, int b) { return a == b; }
 