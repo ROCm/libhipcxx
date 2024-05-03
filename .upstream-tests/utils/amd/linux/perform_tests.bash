264f6c63fc2da5cc10d268d1560c99a9dab9d44d
Philipp Samfass <philipp.samfass@amd.com>
2024-05-03 13:46:25 +0200

    Update perform_tests.bash

--- .upstream-tests/utils/amd/linux/perform_tests.bash
+++ .upstream-tests/utils/amd/linux/perform_tests.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+# Modifications Copyright (c) 2024-2025 Advanced Micro Devices, Inc.
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
@@ -16,7 +16,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-
 
 function usage {
   echo "Usage: ${0} [flags...] <tests...>|all"
