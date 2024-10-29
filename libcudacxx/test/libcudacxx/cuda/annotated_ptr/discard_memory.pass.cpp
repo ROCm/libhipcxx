b38cc62a1155fe2bc18a5e87b257c4c4eb1cd4e6
Wesley Maxey <71408887+wmaxey@users.noreply.github.com>
2024-10-28 23:27:43 -0700

    Fix annotated_ptr test failures. (#2607)
    * Fix failure in annotated_ptr caused by a possible compiler bug when the same function returns different memory spaces at runtime
    
    * Fix double stride jump when applying access property to cache lines
    
    (cherry picked from commit 6f7eaefba19212b05d4d2dd7afef6e765a6cb276)

---
+++ libcudacxx/test/libcudacxx/cuda/annotated_ptr/discard_memory.pass.cpp
@@ -0,0 +1,38 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: pre-sm-70
+// UNSUPPORTED: !nvcc
+// UNSUPPORTED: nvrtc
+// UNSUPPORTED: c++98, c++03
+
+#include "utils.h"
+#define ARR_SZ 128
+
+template <typename T>
+__device__ __host__ __noinline__ void test(bool shared = false)
+{
+  T* arr = global_alloc<T, ARR_SZ>();
+
+  cuda::discard_memory(arr, ARR_SZ);
+
+  dealloc<T>(arr);
+}
+
+__device__ __host__ __noinline__ void test_all()
+{
+  test<int>();
+}
+
+int main(int argc, char** argv)
+{
+  test_all();
+  return 0;
+}
