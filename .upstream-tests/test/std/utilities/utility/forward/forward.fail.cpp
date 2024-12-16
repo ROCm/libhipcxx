529547caa533d9f661a50e18942c9b0b509da60d
Philipp Samfass <philipp.samfass@amd.com>
2024-12-16 07:21:28 -0800

    fix(forward.fail.cpp): do not expect unseen error with hipcc

--- .upstream-tests/test/std/utilities/utility/forward/forward.fail.cpp
+++ .upstream-tests/test/std/utilities/utility/forward/forward.fail.cpp
@@ -7,6 +7,23 @@
 //
 //===----------------------------------------------------------------------===//
 
+// Modifications Copyright (c) 2024-2025 Advanced Micro Devices, Inc.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 // test forward
 
 #include <cuda/std/utility>
@@ -27,11 +44,10 @@ __host__ __device__ const A csource()
 
 int main(int, char**)
 {
-  {
-    (void) cuda::std::forward<A&>(source()); // expected-note {{requested here}}
-    // expected-error-re@__utility/forward.h:* {{{{(static_assert|static assertion)}} failed{{.*}} {{"?}}cannot forward
-    // an rvalue as an lvalue{{"?}}}}
-#if defined(TEST_COMPILER_CLANG) && __clang_major__ > 14
+    {
+    (void) cuda::std::forward<A&>(source());  // expected-note {{requested here}}
+    // expected-error-re@.*__utility/forward.h:* {{{{(static_assert|static assertion)}} failed{{.*}} {{"?}}cannot forward an rvalue as an lvalue{{"?}}}}
+#if defined(TEST_COMPILER_CLANG) && __clang_major__ > 14 && !defined(TEST_COMPILER_HIPCC)  // NOTE(HIP/AMD): hipcc does not emit this error message
     // expected-error {{ignoring return value of function declared with const attribute}}
 #endif
   }
