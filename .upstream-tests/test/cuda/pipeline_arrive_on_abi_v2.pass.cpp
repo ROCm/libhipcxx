d4e661b5eb121fdf665b56eadfddde56ea9938ee
Philipp Samfass <philipp.samfass@amd.com>
2025-01-20 03:45:46 -0800

    doc: update license notice years for modifications

--- .upstream-tests/test/cuda/pipeline_arrive_on_abi_v2.pass.cpp
+++ .upstream-tests/test/cuda/pipeline_arrive_on_abi_v2.pass.cpp
@@ -8,7 +8,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-// Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+// Modifications Copyright (c) 2024-2025 Advanced Micro Devices, Inc.
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
