66f101122c99898b02cba5f47104821f0dbef489
Wesley Maxey <71408887+wmaxey@users.noreply.github.com>
2025-01-28 05:43:17 -0800

    Replace pipes.quote with shlex.quote in lit config (#3547)
    * Replace pipes.quote with shlex.quote
    
    * Drop TBB run on windows to unblock CI
    
    * Update ci/matrix.yaml
    
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>
    Co-authored-by: Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
    (cherry picked from commit e35a0e01c6122c9f8f98083e3ce93effeb1809cc)

--- .upstream-tests/utils/libcudacxx/test/config.py
+++ .upstream-tests/utils/libcudacxx/test/config.py
@@ -25,7 +25,6 @@
 
 import ctypes
 import os
-import pipes
 import platform
 import re
 import shlex
@@ -1682,14 +1681,14 @@ class Configuration(object):
 
     def configure_substitutions(self):
         sub = self.config.substitutions
-        cxx_path = pipes.quote(self.cxx.path)
+        cxx_path = shlex.quote(self.cxx.path)
         # Configure compiler substitutions
         sub.append(("%cxx", cxx_path))
         sub.append(("%libcxx_src_root", self.libcudacxx_src_root))
         # Configure flags substitutions
-        flags_str = " ".join([pipes.quote(f) for f in self.cxx.flags])
-        compile_flags_str = " ".join([pipes.quote(f) for f in self.cxx.compile_flags])
-        link_flags_str = " ".join([pipes.quote(f) for f in self.cxx.link_flags])
+        flags_str = " ".join([shlex.quote(f) for f in self.cxx.flags])
+        compile_flags_str = " ".join([shlex.quote(f) for f in self.cxx.compile_flags])
+        link_flags_str = " ".join([shlex.quote(f) for f in self.cxx.link_flags])
         all_flags = "%s %s %s" % (flags_str, compile_flags_str, link_flags_str)
         sub.append(("%flags", flags_str))
         sub.append(("%compile_flags", compile_flags_str))
@@ -1718,7 +1717,7 @@ class Configuration(object):
         sub.append(("%run", "%t.exe"))
         # Configure not program substitutions
         not_py = os.path.join(self.libcudacxx_src_root, "test", "utils", "not.py")
-        not_str = "%s %s " % (pipes.quote(sys.executable), pipes.quote(not_py))
+        not_str = "%s %s " % (shlex.quote(sys.executable), shlex.quote(not_py))
         sub.append(("not ", not_str))
         if self.get_lit_conf("libcudacxx_gdb"):
             sub.append(("%libcxx_gdb", self.get_lit_conf("libcudacxx_gdb")))
