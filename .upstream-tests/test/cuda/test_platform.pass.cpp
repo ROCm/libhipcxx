2cab6ae18f6cb075a26ab9bc9776c62eb99ca011
Federico Busato <50413820+fbusato@users.noreply.github.com>
2025-01-29 12:49:56 -0800

    Remove all code paths and policies for SM37 and below (#3466)
    (cherry picked from commit fedeefddc937db441e1e5f73308d3d5252aa79d2)

--- .upstream-tests/test/cuda/test_platform.pass.cpp
+++ .upstream-tests/test/cuda/test_platform.pass.cpp
@@ -84,10 +84,6 @@ __host__ __device__ void test()
     (static_assert(arch_val >= 520, "cuda arch expected 520");),
     NV_PROVIDES_SM_50,
     (static_assert(arch_val >= 500, "cuda arch expected 500");),
-    NV_PROVIDES_SM_37,
-    (static_assert(arch_val >= 370, "cuda arch expected 370");),
-    NV_PROVIDES_SM_35,
-    (static_assert(arch_val >= 350, "cuda arch expected 350");),
     NV_IS_HOST,
     (static_assert(arch_val == 0, "cuda arch expected 0");))
 
@@ -117,10 +113,6 @@ __host__ __device__ void test()
     (static_assert(arch_val == 520, "cuda arch expected 520");),
     NV_IS_EXACTLY_SM_50,
     (static_assert(arch_val == 500, "cuda arch expected 500");),
-    NV_IS_EXACTLY_SM_37,
-    (static_assert(arch_val == 370, "cuda arch expected 370");),
-    NV_IS_EXACTLY_SM_35,
-    (static_assert(arch_val == 350, "cuda arch expected 350");),
     NV_IS_HOST,
     (static_assert(arch_val == 0, "cuda arch expected 0");))
 
@@ -177,10 +169,6 @@ __host__ __device__ void test()
     (invoke_count += 1; invoke_count += threadIdx.x;),
     NV_PROVIDES_SM_50,
     (invoke_count += 1; invoke_count += threadIdx.x;),
-    NV_PROVIDES_SM_37,
-    (invoke_count += 1; invoke_count += threadIdx.x;),
-    NV_PROVIDES_SM_35,
-    (invoke_count += 1; invoke_count += threadIdx.x;),
     NV_IS_HOST,
     (invoke_count += 1;))
 
@@ -206,10 +194,6 @@ __host__ __device__ void test()
     NV_IS_EXACTLY_SM_52,
     (invoke_count += 1; invoke_count += threadIdx.x;),
     NV_IS_EXACTLY_SM_50,
-    (invoke_count += 1; invoke_count += threadIdx.x;),
-    NV_IS_EXACTLY_SM_37,
-    (invoke_count += 1; invoke_count += threadIdx.x;),
-    NV_IS_EXACTLY_SM_35,
     (invoke_count += 1; invoke_count += threadIdx.x;),
     NV_IS_HOST,
     (invoke_count += 1;))
@@ -271,10 +255,6 @@ void test()
     (static_assert(arch_val == 520, "cuda arch expected 520");),
     NV_PROVIDES_SM_50,
     (static_assert(arch_val == 500, "cuda arch expected 500");),
-    NV_PROVIDES_SM_37,
-    (static_assert(arch_val == 370, "cuda arch expected 370");),
-    NV_PROVIDES_SM_35,
-    (static_assert(arch_val == 350, "cuda arch expected 350");),
     NV_IS_HOST,
     (static_assert(arch_val == 0, "cuda arch expected 0");))
 
@@ -300,10 +280,6 @@ void test()
     (static_assert(arch_val == 520, "cuda arch expected 520");),
     NV_IS_EXACTLY_SM_50,
     (static_assert(arch_val == 500, "cuda arch expected 500");),
-    NV_IS_EXACTLY_SM_37,
-    (static_assert(arch_val == 370, "cuda arch expected 370");),
-    NV_IS_EXACTLY_SM_35,
-    (static_assert(arch_val == 350, "cuda arch expected 350");),
     NV_IS_HOST,
     (static_assert(arch_val == 0, "cuda arch expected 0");))
 
