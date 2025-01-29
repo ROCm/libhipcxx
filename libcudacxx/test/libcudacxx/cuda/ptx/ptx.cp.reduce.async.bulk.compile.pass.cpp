535afaf939a1361d4b09834019c7159c23775cd7
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-01-29 22:52:12 +0100

    PTX: Update generated files with Blackwell instructions (#3568)
    * ptx: Update existing instructions
    * ptx: Add new instructions
    * Fix returning error out values
    See:
    - https://gitlab-master.nvidia.com/CCCL/libcuda-ptx/-/merge_requests/74
    - https://gitlab-master.nvidia.com/CCCL/libcuda-ptx/-/merge_requests/73
    * ptx: Fix out var declaration
    See  https://gitlab-master.nvidia.com/CCCL/libcuda-ptx/-/merge_requests/75
    * mbarrier.{test,try}_wait: Fix test. Wrong files were included.
    * docs: Fix special registers include
    * Allow non-included documentation pages
    * Workaround NVRTC
    
    Co-authored-by: Allard Hendriksen <ahendriksen@nvidia.com>
    (cherry picked from commit 8063c343945e19817b5f3fb812b737eba8cccfcb)

---
+++ libcudacxx/test/libcudacxx/cuda/ptx/ptx.cp.reduce.async.bulk.compile.pass.cpp
@@ -0,0 +1,32 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+// UNSUPPORTED: libcpp-has-no-threads
+
+// <cuda/ptx>
+
+#include <cuda/ptx>
+#include <cuda/std/utility>
+
+#include "nvrtc_workaround.h"
+// above header needs to be included before the generated test header
+#include "generated/cp_reduce_async_bulk.h"
+
+#ifdef _LIBCUDACXX_HAS_NVF16
+#  include "generated/cp_reduce_async_bulk_f16.h"
+#endif // _LIBCUDACXX_HAS_NVF16
+
+#ifdef _LIBCUDACXX_HAS_NVBF16
+#  include "generated/cp_reduce_async_bulk_bf16.h"
+#endif // _LIBCUDACXX_HAS_NVBF16
+
+int main(int, char**)
+{
+  return 0;
+}
