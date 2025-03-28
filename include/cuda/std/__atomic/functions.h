dcd728ddf4a59522c71ca6d27db86f158bd01957
Michael Obersteiner <Michael.Obersteiner@amd.com>
2025-03-28 03:40:20 -0700

    added hip atomics version again + added include

--- include/cuda/std/__atomic/functions.h
+++ include/cuda/std/__atomic/functions.h
@@ -8,6 +8,23 @@
 //
 //===----------------------------------------------------------------------===//
 
+// Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #ifndef __LIBCUDACXX___ATOMIC_FUNCTIONS_H
 #define __LIBCUDACXX___ATOMIC_FUNCTIONS_H
 
@@ -24,8 +41,14 @@
 #include <cuda/std/__atomic/platform.h>
 
 // Device atomics
+#if defined(__HIP_PLATFORM_AMD__) || defined(__HIPCC_RTC__)
+#include <cuda/std/__atomic/functions/atomic_hip_generated.h>
+#include <cuda/std/__atomic/functions/atomic_hip_derived.h>
+#else
 #include <cuda/std/__atomic/functions/cuda_ptx_derived.h>
 #include <cuda/std/__atomic/functions/cuda_ptx_generated.h>
+#endif
+
 
 // Host atomics
 #include <cuda/std/__atomic/functions/host.h>
