c74832bfbe0a4cbf325e45c0ea3ad6662147d47f
Michael Obersteiner <Michael.Obersteiner@amd.com>
2025-03-31 09:21:06 -0700

    fixed #ifdef that was not executed for the hip platform AMD

--- include/cuda/std/__atomic/wait/notify_wait.h
+++ include/cuda/std/__atomic/wait/notify_wait.h
@@ -8,6 +8,23 @@
 //
 //===----------------------------------------------------------------------===//
 
+// Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #ifndef _LIBCUDACXX___ATOMIC_WAIT_NOTIFY_WAIT_H
 #define _LIBCUDACXX___ATOMIC_WAIT_NOTIFY_WAIT_H
 
@@ -53,7 +70,7 @@ _LIBCUDACXX_HIDE_FROM_ABI void __atomic_notify_all(_Tp const volatile*, _Sco)
 template <typename _Tp>
 _LIBCUDACXX_HIDE_FROM_ABI bool __nonatomic_compare_equal(_Tp const& __lhs, _Tp const& __rhs)
 {
-#if defined(_CCCL_CUDA_COMPILER)
+#if defined(_CCCL_CUDA_COMPILER) || defined(__HIP_PLATFORM_AMD__) || defined(__HIPCC_RTC__)
   return __lhs == __rhs;
 #else
   return memcmp(&__lhs, &__rhs, sizeof(_Tp)) == 0;
