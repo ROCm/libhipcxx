a3a7413b67d5f9f3c27d852477a89ca2c8286667
Michael Obersteiner <Michael.Obersteiner@amd.com>
2025-04-01 07:29:48 -0700

    FIXME related to #60: disabled test that are not passing due to bug with atomic fetch operations used in ManagedMemory

--- .upstream-tests/test/heterogeneous/atomic/reference_cuda.pass.cpp
+++ .upstream-tests/test/heterogeneous/atomic/reference_cuda.pass.cpp
@@ -6,6 +6,23 @@
 //
 //===----------------------------------------------------------------------===//
 
+// Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 // UNSUPPORTED: nvrtc, pre-sm-60
 // UNSUPPORTED: windows && pre-sm-70
 
@@ -167,11 +184,14 @@ using arithmetic_atomic_testers =
 
 using bitwise_atomic_testers =
   append<arithmetic_atomic_testers,
-         fetch_add_tester<-8, 10, 2>,
-         fetch_or_tester<2, 13, 15>,
+         fetch_add_tester<-8, 10, 2>/*,
+         //FIXME(HIP): fetch_(or/xor/and) do not return the correct previous value when the atomic is allocated with hipMallocManaged.
+         //This issue is tracked internally in issue SWDEV-390383. 
+         fetch_or_tester<2, 13, 15>
          fetch_and_tester<15, 8, 8>,
          fetch_and_tester<8, 13, 8>,
-         fetch_xor_tester<8, 12, 4>>;
+         fetch_xor_tester<8, 12, 4>*/
+         >;
 
 void kernel_invoker()
 {
