3275fde1cdb359440b215709c636158f09c856e0
Michael Obersteiner <Michael.Obersteiner@amd.com>
2025-04-25 02:26:29 -0700

    feat/fix: enable usage of atomic wait functionality for AMD devices. This fixes a segmentation fault issue in .upstream-tests/test/std/atomics/atomics.types.operations/atomics.types.operations.wait/atomic_wait.pass.cpp test. TODO: Further investigation should be done to check why fallback did not work.

--- include/cuda/std/__atomic/wait/notify_wait.h
+++ include/cuda/std/__atomic/wait/notify_wait.h
@@ -50,9 +50,13 @@ template <typename _Tp, typename _Sco>
 _LIBCUDACXX_HIDE_FROM_ABI void
 __atomic_try_wait_slow(_Tp const volatile* __a, __atomic_underlying_remove_cv_t<_Tp> __val, memory_order __order, _Sco)
 {
+  #ifdef __HIP_PLATFORM_AMD__
+  __atomic_try_wait_slow_fallback(__a, __val, __order, _Sco{});
+  #else
   NV_DISPATCH_TARGET(NV_PROVIDES_SM_70, __atomic_try_wait_slow_fallback(__a, __val, __order, _Sco{});
                      , NV_IS_HOST, __atomic_try_wait_slow_fallback(__a, __val, __order, _Sco{});
                      , NV_ANY_TARGET, __atomic_try_wait_unsupported_before_SM_70__(););
+  #endif
 }
 
 template <typename _Tp, typename _Sco>
