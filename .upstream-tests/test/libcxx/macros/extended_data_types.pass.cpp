6157b360480b2abffb18a0da67911424919b75b1
Michael Obersteiner <Michael.Obersteiner@amd.com>
2025-11-24 13:53:58 +0000

    fix: include amd extended data type headers for various data types

--- .upstream-tests/test/libcxx/macros/extended_data_types.pass.cpp
+++ .upstream-tests/test/libcxx/macros/extended_data_types.pass.cpp
@@ -6,9 +6,51 @@
 // SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
 //
 //===----------------------------------------------------------------------===//
+
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <cuda/std/__cccl/extended_data_types.h>
 
 #include "test_macros.h"
+#ifdef __HIP_PLATFORM_AMD__
+
+#if _CCCL_HAS_NVFP4()
+#  include <hip/hip_fp4.h>
+#endif
+#if _CCCL_HAS_NVFP6_E2M3()
+#  include <hip/hip_fp6.h>
+#endif
+#if _CCCL_HAS_NVFP8()
+#  include <hip/hip_fp8.h>
+#endif
+#if _CCCL_HAS_NVFP16()
+#  include <hip/hip_fp16.h>
+#endif
+#if _CCCL_HAS_NVBF16()
+#  include <hip/hip_bf16.h>
+#endif
+
+#else
 
 #if _CCCL_HAS_NVFP4()
 #  include <cuda_fp4.h>
@@ -24,6 +66,8 @@
 #endif
 #if _CCCL_HAS_NVBF16()
 #  include <cuda_bf16.h>
+#endif
+
 #endif
 
 template <class T>
