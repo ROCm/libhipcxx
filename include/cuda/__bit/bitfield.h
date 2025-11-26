c0e0f2408fcb007ebd947b09fcad5d3053182b32
Michael Obersteiner <Michael.Obersteiner@amd.com>
2025-11-26 15:33:58 +0000

    tmpfix: for now remove bfe and bfi builtins as they do not exist or work properly within libhipcxx

--- include/cuda/__bit/bitfield.h
+++ include/cuda/__bit/bitfield.h
@@ -8,6 +8,28 @@
 //
 //===----------------------------------------------------------------------===//
 
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #ifndef _CUDA___BIT_BITFILED_INSERT_EXTRACT_H
 #define _CUDA___BIT_BITFILED_INSERT_EXTRACT_H
 
@@ -49,16 +71,20 @@ __bfi(uint64_t __dest, uint64_t __source, int __start, int __width) noexcept
 
 _CCCL_NODISCARD _CCCL_HIDE_FROM_ABI _CCCL_DEVICE uint32_t __bfe(uint32_t __value, int __start, int __width) noexcept
 {
-  uint32_t __ret;
-  asm("bfe.u32 %0, %1, %2, %3;" : "=r"(__ret) : "r"(__value), "r"(__start), "r"(__width));
-  return __ret;
+  // uint32_t __ret;
+  // asm("bfe.u32 %0, %1, %2, %3;" : "=r"(__ret) : "r"(__value), "r"(__start), "r"(__width));
+  // return __ret;
+  // TODO(HIP/AMD): this builtin seems to not work correctly/as intended here atm
+  return __builtin_amdgcn_ubfe(__value, __start, __width);
 }
 
 _CCCL_NODISCARD _CCCL_HIDE_FROM_ABI _CCCL_DEVICE uint64_t __bfe(uint64_t __value, int __start, int __width) noexcept
 {
-  uint64_t __ret;
-  asm("bfe.u64 %0, %1, %2, %3;" : "=l"(__ret) : "l"(__value), "r"(__start), "r"(__width));
-  return __ret;
+  // uint64_t __ret;
+  // asm("bfe.u64 %0, %1, %2, %3;" : "=l"(__ret) : "l"(__value), "r"(__start), "r"(__width));
+  // return __ret;
+  // TODO(HIP/AMD): this builtin seems to not work correctly/as intended here atm
+  return __builtin_amdgcn_ubfe(__value, __start, __width);
 }
 
 #endif // __cccl_ptx_isa >= 200
@@ -72,6 +98,7 @@ bitfield_insert(const _Tp __dest, const _Tp __source, int __start, int __width) 
   _CCCL_ASSERT(__width >= 0 && __width <= __digits, "width out of range");
   _CCCL_ASSERT(__start >= 0 && __start <= __digits, "start position out of range");
   _CCCL_ASSERT(__start + __width <= __digits, "start position + width out of range");
+#ifndef __HIP_PLATFORM_AMD__
   if constexpr (sizeof(_Tp) <= sizeof(uint64_t))
   {
     if (!_CUDA_VSTD::__cccl_default_is_constant_evaluated())
@@ -85,6 +112,7 @@ bitfield_insert(const _Tp __dest, const _Tp __source, int __start, int __width) 
       // clang-format on
     }
   }
+#endif
   auto __mask = ::cuda::bitmask<_Tp>(__start, __width);
   return (::cuda::__shl(__source, __start) & __mask) | (__dest & ~__mask);
 }
@@ -98,6 +126,7 @@ bitfield_extract(const _Tp __value, int __start, int __width) noexcept
   _CCCL_ASSERT(__width >= 0 && __width <= __digits, "width out of range");
   _CCCL_ASSERT(__start >= 0 && __start <= __digits, "start position out of range");
   _CCCL_ASSERT(__start + __width <= __digits, "start position + width out of range");
+#ifndef __HIP_PLATFORM_AMD__
   if constexpr (sizeof(_Tp) <= sizeof(uint32_t))
   {
     if (!_CUDA_VSTD::__cccl_default_is_constant_evaluated())
@@ -110,6 +139,7 @@ bitfield_extract(const _Tp __value, int __start, int __width) noexcept
       // clang-format on
     }
   }
+#endif
   return ::cuda::__shr(__value, __start) & ::cuda::bitmask<_Tp>(0, __width);
 }
 
