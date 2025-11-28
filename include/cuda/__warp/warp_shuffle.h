2836359ad8a2683baeb05f6ccfdeeb92b06f3ae2
Michael Obersteiner <Michael.Obersteiner@amd.com>
2025-11-28 18:43:23 +0000

    fix: move ptx includes into Nvidia ptx guard

--- include/cuda/__warp/warp_shuffle.h
+++ include/cuda/__warp/warp_shuffle.h
@@ -8,6 +8,28 @@
 //
 //===----------------------------------------------------------------------===//
 
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #ifndef _CUDA___WARP_WARP_SHUFFLE_H
 #define _CUDA___WARP_WARP_SHUFFLE_H
 
@@ -22,8 +44,6 @@
 #endif // no system header
 
 #include <cuda/__cmath/ceil_div.h>
-#include <cuda/__ptx/instructions/get_sreg.h>
-#include <cuda/__ptx/instructions/shfl_sync.h>
 #include <cuda/std/__bit/has_single_bit.h>
 #include <cuda/std/__concepts/concept_macros.h>
 #include <cuda/std/__memory/addressof.h>
@@ -35,6 +55,8 @@
 #include <cuda/std/cstdint>
 
 #if __cccl_ptx_isa >= 600
+#include <cuda/__ptx/instructions/get_sreg.h>
+#include <cuda/__ptx/instructions/shfl_sync.h>
 
 _LIBCUDACXX_BEGIN_NAMESPACE_CUDA_DEVICE
 
