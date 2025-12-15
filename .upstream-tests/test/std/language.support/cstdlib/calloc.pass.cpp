fc9770d8bc2801d8948a3ef8cfd63b1ad57ce250
Michael Obersteiner <Michael.Obersteiner@amd.com>
2025-12-15 13:22:36 +0000

    tmpfix(#127): unsupport test_calloc_fail as calloc currently might return incorrect results for invalid sizes on AMD hardware

--- .upstream-tests/test/std/language.support/cstdlib/calloc.pass.cpp
+++ .upstream-tests/test/std/language.support/cstdlib/calloc.pass.cpp
@@ -7,6 +7,28 @@
 //
 //===----------------------------------------------------------------------===//
 
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <cuda/std/cassert>
 #include <cuda/std/cstdint>
 #include <cuda/std/cstdlib>
@@ -91,7 +113,10 @@ __host__ __device__ void test()
   test_calloc_success<BigStruct>(4);
   test_calloc_success<AlignedStruct>(16);
 
+  // NOTE(HIP/AMD): calloc currently misbehaves for large input sizes and does not return nullptr.
+  #ifndef __HIP_PLATFORM_AMD__
   test_calloc_fail<int>(cuda::std::numeric_limits<cuda::std::size_t>::max());
+  #endif
 }
 
 int main(int, char**)
