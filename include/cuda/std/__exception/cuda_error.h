NVIDIA/cccl

Add `cuda::get_device_address` (#2611)
Its a pain to retrieve the address of a device symbol.

This adds a little helper that makes it less painfull

libcudacxx/include/cuda/std/__exception/cuda_error.h
@@ -72,7 +72,7 @@ _CCCL_NORETURN _LIBCUDACXX_HIDE_FROM_ABI void __throw_cuda_error(::cudaError_t _
 class cuda_error
 {
 public:
-  cuda_error(::cudaError_t, const char*) noexcept {}
+  _LIBCUDACXX_HIDE_FROM_ABI cuda_error(::cudaError_t, const char*) noexcept {}
 };
 
 _CCCL_NORETURN _LIBCUDACXX_HIDE_FROM_ABI void __throw_cuda_error(::cudaError_t, const char*)

ROCm/libhipcxx

(cherry picked from commit 937853d320740d19efb71d05575b27d961583c64)

include/cuda/std/__exception/cuda_error.h
@@ -89,7 +89,7 @@ _CCCL_NORETURN _LIBCUDACXX_HIDE_FROM_ABI void __throw_cuda_error(::hipError_t __
 class cuda_error
 {
 public:
-  cuda_error(::hipError_t, const char*) noexcept {}
+  _LIBCUDACXX_HIDE_FROM_ABI cuda_error(::hipError_t, const char*) noexcept {}
 };
 
 _CCCL_NORETURN _LIBCUDACXX_HIDE_FROM_ABI void __throw_cuda_error(::hipError_t, const char*)