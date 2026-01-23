NVIDIA/cccl

add a config macro for testing support for inline variables (#2581)
* add a config macro for testing support for inline variables

* Drop `_LIBCUDACXX_` macros

* Use a consistent macro for enabling variable templates

* Move it in repo.toml

---------

Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>

libcudacxx/include/cuda/std/__type_traits/is_extended_floating_point.h
@@ -28,13 +28,15 @@ template <class _Tp>
 struct __is_extended_floating_point : false_type
 {};
 
-#if _CCCL_STD_VER >= 2017 && defined(__cpp_inline_variables) && (__cpp_inline_variables >= 201606L)
+#if !defined(_CCCL_NO_VARIABLE_TEMPALTES)
 template <class _Tp>
-_LIBCUDACXX_INLINE_VAR constexpr bool __is_extended_floating_point_v = false;
-#elif _CCCL_STD_VER >= 2014 && !defined(_LIBCUDACXX_HAS_NO_VARIABLE_TEMPLATES)
-template <class _Tp>
-_LIBCUDACXX_INLINE_VAR constexpr bool __is_extended_floating_point_v = __is_extended_floating_point<_Tp>::value;
-#endif // _CCCL_STD_VER >= 2014
+_CCCL_INLINE_VAR constexpr bool __is_extended_floating_point_v
+#  if defined(_CCCL_NO_INLINE_VARIABLES)
+  = __is_extended_floating_point<_Tp>::value;
+#  else // ^^^ _CCCL_NO_INLINE_VARIABLES ^^^ / vvv !_CCCL_NO_INLINE_VARIABLES vvv
+  = false;
+#  endif // !_CCCL_NO_INLINE_VARIABLES
+#endif // !_CCCL_NO_VARIABLE_TEMPALTES
 
 #if defined(_LIBCUDACXX_HAS_NVFP16)
 #  include <cuda_fp16.h>
@@ -43,10 +45,10 @@ template <>
 struct __is_extended_floating_point<__half> : true_type
 {};
 
-#  if _CCCL_STD_VER >= 2017 && defined(__cpp_inline_variables) && (__cpp_inline_variables >= 201606L)
+#  ifndef _CCCL_NO_INLINE_VARIABLES
 template <>
-_LIBCUDACXX_INLINE_VAR constexpr bool __is_extended_floating_point_v<__half> = true;
-#  endif // _CCCL_STD_VER >= 2014
+_CCCL_INLINE_VAR constexpr bool __is_extended_floating_point_v<__half> = true;
+#  endif // !_CCCL_NO_INLINE_VARIABLES
 #endif // _LIBCUDACXX_HAS_NVFP16
 
 #if defined(_LIBCUDACXX_HAS_NVBF16)
@@ -59,10 +61,10 @@ template <>
 struct __is_extended_floating_point<__nv_bfloat16> : true_type
 {};
 
-#  if _CCCL_STD_VER >= 2017 && defined(__cpp_inline_variables) && (__cpp_inline_variables >= 201606L)
+#  ifndef _CCCL_NO_INLINE_VARIABLES
 template <>
-_LIBCUDACXX_INLINE_VAR constexpr bool __is_extended_floating_point_v<__nv_bfloat16> = true;
-#  endif // _CCCL_STD_VER >= 2014
+_CCCL_INLINE_VAR constexpr bool __is_extended_floating_point_v<__nv_bfloat16> = true;
+#  endif // !_CCCL_NO_INLINE_VARIABLES
 #endif // _LIBCUDACXX_HAS_NVBF16
 
 _LIBCUDACXX_END_NAMESPACE_STD

ROCm/libhipcxx

(cherry picked from commit dabc1b74417323365e1fc7cdb09fdef84180e7ff)

include/cuda/std/__type_traits/is_extended_floating_point.h
@@ -45,13 +45,15 @@ template <class _Tp>
 struct __is_extended_floating_point : false_type
 {};
 
-#if _CCCL_STD_VER >= 2017 && defined(__cpp_inline_variables) && (__cpp_inline_variables >= 201606L)
+#if !defined(_CCCL_NO_VARIABLE_TEMPALTES)
 template <class _Tp>
-_LIBCUDACXX_INLINE_VAR constexpr bool __is_extended_floating_point_v = false;
-#elif _CCCL_STD_VER >= 2014 && !defined(_LIBCUDACXX_HAS_NO_VARIABLE_TEMPLATES)
-template <class _Tp>
-_LIBCUDACXX_INLINE_VAR constexpr bool __is_extended_floating_point_v = __is_extended_floating_point<_Tp>::value;
-#endif // _CCCL_STD_VER >= 2014
+_CCCL_INLINE_VAR constexpr bool __is_extended_floating_point_v
+#  if defined(_CCCL_NO_INLINE_VARIABLES)
+  = __is_extended_floating_point<_Tp>::value;
+#  else // ^^^ _CCCL_NO_INLINE_VARIABLES ^^^ / vvv !_CCCL_NO_INLINE_VARIABLES vvv
+  = false;
+#  endif // !_CCCL_NO_INLINE_VARIABLES
+#endif // !_CCCL_NO_VARIABLE_TEMPALTES
 
 #if defined(_LIBCUDACXX_HAS_NVFP16)
 
@@ -65,10 +67,10 @@ template <>
 struct __is_extended_floating_point<__half> : true_type
 {};
 
-#  if _CCCL_STD_VER >= 2017 && defined(__cpp_inline_variables) && (__cpp_inline_variables >= 201606L)
+#  ifndef _CCCL_NO_INLINE_VARIABLES
 template <>
-_LIBCUDACXX_INLINE_VAR constexpr bool __is_extended_floating_point_v<__half> = true;
-#  endif // _CCCL_STD_VER >= 2014
+_CCCL_INLINE_VAR constexpr bool __is_extended_floating_point_v<__half> = true;
+#  endif // !_CCCL_NO_INLINE_VARIABLES
 #endif // _LIBCUDACXX_HAS_NVFP16
 
 #if defined(_LIBCUDACXX_HAS_NVBF16)
@@ -85,10 +87,10 @@ template <>
 struct __is_extended_floating_point<__hip_bfloat16> : true_type
 {};
 
-#  if _CCCL_STD_VER >= 2017 && defined(__cpp_inline_variables) && (__cpp_inline_variables >= 201606L)
+#  ifndef _CCCL_NO_INLINE_VARIABLES
 template <>
-_LIBCUDACXX_INLINE_VAR constexpr bool __is_extended_floating_point_v<__hip_bfloat16> = true;
-#  endif // _CCCL_STD_VER >= 2014
+_CCCL_INLINE_VAR constexpr bool __is_extended_floating_point_v<__hip_bfloat16> = true;
+#  endif // !_CCCL_NO_INLINE_VARIABLES
 #endif // _LIBCUDACXX_HAS_NVBF16
 
 _LIBCUDACXX_END_NAMESPACE_STD