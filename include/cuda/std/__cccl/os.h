fe4d0206e8ec59ee7d5f5d662e0e0e0260ff9d57
Michael Obersteiner <Michael.Obersteiner@amd.com>
2026-02-19 14:17:06 +0000

    fix(Windows): correct logic on Windows to not falsly detect operating as Linux when using hipcc.

--- include/cuda/std/__cccl/os.h
+++ include/cuda/std/__cccl/os.h
@@ -8,6 +8,28 @@
 //
 //===----------------------------------------------------------------------===//
 
+// MIT License
+//
+// Modifications Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #ifndef __CCCL_OS_H
 #define __CCCL_OS_H
 
@@ -25,7 +47,7 @@
 #  define _CCCL_OS_WINDOWS_() 0
 #endif
 
-#if defined(__linux__) || defined(__LP64__) /* __LP64__ for NVRTC */
+#if defined(__linux__) || ((defined(__CUDACC_RTC__) || defined(__HIPCC_RTC__)) && defined(__LP64__))
 #  define _CCCL_OS_LINUX_() 1
 #else
 #  define _CCCL_OS_LINUX_() 0
