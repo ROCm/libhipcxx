3f3d95a24f302ee41f5e4c524a03b32ce63e82e8
Michael Obersteiner <Michael.Obersteiner@amd.com>
2026-02-19 14:03:34 +0000

    fix: add _CCCL_DECLSPEC_EMPTY_BASES to basic_resource_ref to allow to enforce proper empty base optimization

--- include/cuda/__memory_resource/resource_ref.h
+++ include/cuda/__memory_resource/resource_ref.h
@@ -8,6 +8,28 @@
 //
 //===----------------------------------------------------------------------===//
 
+// MIT License
+//
+// Modifications Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #ifndef _CUDA__MEMORY_RESOURCE_RESOURCE_REF_H
 #define _CUDA__MEMORY_RESOURCE_RESOURCE_REF_H
 
@@ -476,7 +498,7 @@ struct _Resource_ref_helper
 };
 
 template <_AllocType _Alloc_type, class... _Properties>
-class basic_resource_ref
+class _CCCL_DECLSPEC_EMPTY_BASES basic_resource_ref
     : public _Resource_base<_Alloc_type, _WrapperType::_Reference>
     , private _Filtered_vtable<_Properties...>
 {
