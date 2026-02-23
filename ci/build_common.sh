19614885d0ef8f58030bf92af9b4c93dc1e3dd6d
Philipp Samfass <philipp.samfass@amd.com>
2026-02-23 17:48:25 +0000

    fix: set LLVM_PATH for HIPRTC/COMGR in CI environments
    Set LLVM_PATH=/opt/rocm/lib/llvm to ensure COMGR locates the correct
    Clang binary for compilation. In some environments like manylinux base
    images, COMGR defaults to /bin/clang when LLVM_PATH is unset, which
    can cause incorrect GCC version detection and compilation errors.

--- ci/build_common.sh
+++ ci/build_common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Modifications Copyright (c) 2024-2025 Advanced Micro Devices, Inc.
+# Modifications Copyright (c) 2024-2026 Advanced Micro Devices, Inc.
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
